249de49815cafa0c2db273f8c48e458ce0b7a264
Ivan <ivan.frontcoder@gmail.com>
2022-05-04 17:57:41 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -777,12 +777,9 @@ $(document).ready(function () {
 			prevEl: '.vacancies__arrow--prev',
 		},
 		breakpoints: {
-			500: {
-				slidesPerView: 2,
-				spaceBetween: 20
-			},
 			768: {
 				slidesPerView: 3,
+				spaceBetween: 20
 			}
 		}
 	});
